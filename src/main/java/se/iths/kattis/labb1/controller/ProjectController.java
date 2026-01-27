dbeb9bac9a57d174e8da80b1cd61e122a370a051
Ditt github-maryam12370 <Din github-maryamselemankhil786@gmail.com>
2026-01-27 13:37:35 +0100

    added project

--- src/main/java/se/iths/kattis/labb1/controller/ProjectController.java
+++ src/main/java/se/iths/kattis/labb1/controller/ProjectController.java
@@ -3,16 +3,16 @@ package se.iths.kattis.labb1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import se.iths.kattis.labb1.model.Project;
 import se.iths.kattis.labb1.service.ProjectService;
 
 @Controller
+@RequestMapping("/")
 public class ProjectController {
     private final ProjectService projectService;
 
-    private Project project;
-
-    public ProjectController(ProjectController projectController) {
+    public ProjectController(ProjectService projectService) {
         this.projectService = projectService;
     }
 
