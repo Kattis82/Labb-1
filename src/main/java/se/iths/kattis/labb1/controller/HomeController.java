cfa14481929b502f6caa751b89ad64136665af19
Kattis82 <kattiscalmvik@outlook.com>
2026-01-27 09:51:57 +0100

    New project created - Labb 1

---
+++ src/main/java/se/iths/kattis/labb1/controller/HomeController.java
@@ -0,0 +1,17 @@
+package se.iths.kattis.labb1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String home(Model model) {
+        model.addAttribute("name", "Kattis");
+        return "index";
+    }
+}
---
+++ src/test/java/se/iths/kattis/labb1/Labb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.kattis.labb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labb1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
